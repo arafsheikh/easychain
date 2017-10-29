47bb2b012a4aeea28ad050471aded860ea6290e9
Vijay Nandwani <born2bgeeky@gmail.com>
2017-10-29 13:32:34 +0530

    venv

--- api.py
+++ api.py
@@ -1,17 +1,26 @@
-import jsonify
+import jsonify, os, imagehash
+from PIL import Image
+from uuid import uuid4
 
-from flask import Flask, request
+from werkzeug.utils import secure_filename
+
+from flask import Flask, request, render_template, jsonify
 from blockchain import Blockchain, Block, Message
 
 chain = None
 app = Flask(__name__)
-
+app.config['DEBUG'] = True
+UPLOAD_FOLDER = os.path.basename('uploads')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def create_app():
     global chain
     chain = Blockchain()
     return app
 
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
 
 @app.route('/addPatient', methods=['POST'])
 def add_patient():
@@ -29,25 +38,33 @@ def add_patient():
     block.add_message(Message(genetic_problems))
     block.seal()
 
-    chain.add_block(block)
+    #chain.add_block(block)
 
     return jsonify({'status': 'success'})
+
+
 
 
 @app.route('/addRecord', methods=['POST'])
 def add_record():
     digital_id = request.form['id']
-    rec_type = request.form['record_type']
-    hash = request.form['hash']
+    record_type = request.form['record_type']
     accessible_by = request.form['accessible_by']
     datacenter = request.form['datacenter']
+    image_file = request.files['image_file']
+    image_extension = os.path.splitext(secure_filename(image_file.filename))[1]
+    unique_filename = uuid4().hex + image_extension
+    image_file.save(os.path.join(app.config['UPLOAD_FOLDER'], unique_filename))
+    hash = str(imagehash.average_hash(Image.open(os.path.join(app.config['UPLOAD_FOLDER'], unique_filename))))
+
 
     block = Block()
     block.add_message(Message(digital_id))
-    block.add_message(Message(rec_type))
+    block.add_message(Message(record_type))
     block.add_message(Message(hash))
     block.add_message(Message(accessible_by))
     block.add_message(Message(datacenter))
+    block.add_message(Message(unique_filename))
     block.seal()
     chain.add_block(block)
 
